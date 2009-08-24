b6855455de8b6ebfb2ff360f5c4e36a44ec7a565
Ryan Coyner <rcoyner@gmail.com>
2009-08-25 02:00:49 +0800

    Removed ``$`` symbols entirely to allow multiple JavaScript libraries and frameworks. This fixes a compatability issue with RichFaces.
    Passes relevant lint tests using JSLint:
    http://jslint.com/
    
    Refactored the development code to adhere to these standards:
    http://javascript.crockford.com/code.html
    
    Signed-off-by: Alexandr Zykov <alexandrz@gmail.com>

--- jquery.repeatedclick.js
+++ jquery.repeatedclick.js
@@ -1,52 +1,50 @@
-/**
- * jQuery repeatedclick
- *
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- *
- * Written by: Alexandr Zykov <alexandrz@gmail.com>
- *
- * Repeated events if holding mouse button
- *
- */
+jQuery.fn.repeatedclick = function (f, options) {
+    var defaults = {
+        duration  : 350,
+        speed     : 0.85,
+        min       : 50
+    };
 
-$.fn.repeatedclick = function(f, options) {
-  var defaults = {
-    duration  : 350, 
-    speed     : 0.85,
-    min       : 50
-  };
-  var opts = $.extend(defaults, options);
-  
-  if (typeof jQuery.repeatedEvents == 'undefined')
-    jQuery.repeatedEvents = [];
-  if (typeof jQuery.repeatedElements == 'undefined')
-    jQuery.repeatedElements = [];
-  
-  jQuery.repeatedEvents.push(f)
-  jQuery.repeatedElements.push(this)
-  
-  var eventNum = jQuery.repeatedEvents.length - 1;
- 
-  return this.each(function () {
-    
-    repeatedEvent = function (eventNum, duration) {
-      jQuery.repeatedElements[eventNum].each(jQuery.repeatedEvents[eventNum])
-      repeatedEventTimer = setTimeout(
-        'repeatedEvent('+eventNum+', '+(duration > opts.min ? duration * opts.speed : duration)+')', duration)
-    }
-    
-    jQuery(this).mousedown(function() {
-      jQuery.repeatedEventDuration = opts.duration;
-      repeatedEvent(eventNum, opts.duration)
-    })
+    var opts = jQuery.extend(defaults, options);
 
-  	var clearRepeatedEvent = function() {
-    	if (typeof repeatedEventTimer != 'undefined')
-    	  clearInterval(repeatedEventTimer)
+    if (typeof jQuery.repeatedEvents === 'undefined') {
+        jQuery.repeatedEvents = [];
     }
 
-    jQuery(this).mouseout(clearRepeatedEvent);
-    jQuery(this).mouseup(clearRepeatedEvent);
-  })
-}
+    if (typeof jQuery.repeatedElements === 'undefined') {
+        jQuery.repeatedElements = [];
+    }
+
+    jQuery.repeatedEvents.push(f);
+    jQuery.repeatedElements.push(this);
+
+    var eventNum = jQuery.repeatedEvents.length - 1;
+
+    return this.each(function () {
+        repeatedEvent = function (eventNum, duration) {
+            jQuery.repeatedElements[eventNum].each(
+                jQuery.repeatedEvents[eventNum]
+            );
+
+            repeatedEventTimer = setTimeout(
+                'repeatedEvent(' + eventNum + ', ' +
+                (duration > opts.min ? duration * opts.speed : duration) + ')',
+                duration
+            );
+        };
+
+        jQuery(this).mousedown(function () {
+            jQuery.repeatedEventDuration = opts.duration;
+            repeatedEvent(eventNum, opts.duration);
+        });
+
+        var clearRepeatedEvent = function () {
+            if (typeof repeatedEventTimer !== 'undefined') {
+                clearInterval(repeatedEventTimer);
+            }
+        };
+
+        jQuery(this).mouseout(clearRepeatedEvent);
+        jQuery(this).mouseup(clearRepeatedEvent);
+    });
+};
